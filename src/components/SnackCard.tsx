4ca83c15bb7f06c55d55aec26e7f807c4da50e00
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-09-23 14:17:30 +0300

    Add google search for snacks

--- src/components/SnackCard.tsx
+++ src/components/SnackCard.tsx
@@ -4,18 +4,25 @@ interface SnackCardProps {
   snack: Snack;
 }
 
+const googleSearch = (snackName: string) => {
+  window.open(`https://www.google.com/search?q=${snackName}&tbm=shop`, "_blank,noopener,noreferrer");
+};
+
 function SnackCard({ snack }: SnackCardProps) {
   return (
     <div className="snack-card">
       <div className="snack-card__category">
-        {snack.category}{" "}
-        <img width="30px" src={`img/${snack.category}.svg`} alt="" />
+        <span>{snack.categoryName}</span>
+        <img width="30px" src={`img/${snack.categoryName}.svg`} alt="" />
       </div>
-      <img className="snack-card__img" src={`img/chat-kola.webp`} alt="" />
+      <img className="snack-card__img" src={snack.imageUrl} alt="" />
       <div className="snack-card__title">{snack.name}</div>
-      <a className="snack-card__button" href="/">
+      <button
+        className="snack-card__button"
+        onClick={() => googleSearch(snack.name)}
+      >
         Hala satışta mı?
-      </a>
+      </button>
     </div>
   );
 }
