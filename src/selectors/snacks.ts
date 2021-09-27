983ef73e7abe0bb3eb992fd3673f6c2182982a11
Emirhan KARAHAN <emirhan.karahan@outlook.com>
2021-09-27 11:33:38 +0300

    Update selector's return type

--- src/selectors/snacks.ts
+++ src/selectors/snacks.ts
@@ -1,23 +1,29 @@
 import { FilterState } from "../features/filtersSlice";
 import { Snack } from "../features/snacksSlice";
 
-function selectSnacks(snacks: Snack[], { text, sortBy }: FilterState): Snack[] {
-  return snacks
-    .filter((snack) => {
-      let filterText = text.toLowerCase();
-      const textMatch =
-        snack.name.toLowerCase().includes(filterText) ||
-        snack.categoryName.toLowerCase().includes(filterText);
-      return textMatch;
-    })
-    .sort((a, b) => {
-      if (sortBy === "name") {
-        return a.name < b.name ? 1 : -1;
-      } else if (sortBy === "categoryName") {
-        return a.categoryName < b.categoryName ? 1 : -1;
-      }
-      return -1;
-    });
+function selectSnacks(
+  snacks: Snack[] | null,
+  { text, sortBy }: FilterState
+): Snack[] | null {
+  if (snacks) {
+    return snacks
+      .filter((snack) => {
+        let filterText = text.toLowerCase();
+        const textMatch =
+          snack.name.toLowerCase().includes(filterText) ||
+          snack.categoryName.toLowerCase().includes(filterText);
+        return textMatch;
+      })
+      .sort((a, b) => {
+        if (sortBy === "name") {
+          return a.name < b.name ? 1 : -1;
+        } else if (sortBy === "categoryName") {
+          return a.categoryName < b.categoryName ? 1 : -1;
+        }
+        return -1;
+      });
+  }
+  return null;
 }
 
 export default selectSnacks;
